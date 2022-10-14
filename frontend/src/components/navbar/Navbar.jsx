0da139e29169864fc6eaf0f171ac8ecb110df7d8
Anshul-Singla <asingla2807@gmail.com>
2022-10-14 17:07:55 +0530

    frontend setup

---
+++ frontend/src/components/navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import  './navbar.css'
+const Navbar = () => {
+  // console.log('style:', style)
+  return (
+    <div className="navbar">
+        <p>Home</p>
+        <p>Sign in</p>
+        <p>Sign up</p>
+    </div>
+  )
+}
+
+export default Navbar
